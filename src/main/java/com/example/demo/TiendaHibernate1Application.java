8c380bc46377ec42fee4fbb5843561b82e2fbc6c
Kinitto <joaquingomezinfor@gmail.com>
2022-02-04 12:41:19 +0100

    war

--- src/main/java/com/example/demo/TiendaHibernate1Application.java
+++ src/main/java/com/example/demo/TiendaHibernate1Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TiendaHibernate1Application {
+public class TiendaHibernate1Application extends SpringBootServletInitializer{
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TiendaHibernate1Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaHibernate1Application.class, args);
 	}
