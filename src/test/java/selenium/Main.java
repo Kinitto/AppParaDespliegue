f9fc2d5d51818f057c8aa038425f785334717ea7
Joaquín Gómez <joaquingomezinfor@gmail.com>
2022-02-28 20:09:22 +0100

    SeleniumTest

---
+++ src/test/java/selenium/Main.java
@@ -0,0 +1,56 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.edge.driver", "./drivers/msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		
+		try {
+			driver.get("http://localhost:8080/");
+			Thread.sleep(1000);
+			driver.findElement(By.id("username")).sendKeys("Admin");
+			Thread.sleep(1000);
+			driver.findElement(By.id("password")).sendKeys("1234");
+			Thread.sleep(1000);
+			driver.findElement(By.id("btnLogin")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.className("three")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.className("continuar")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.className("two")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.id("cantidadCamiseta")).click();
+			driver.findElement(By.id("cantidadCamiseta")).sendKeys("2");
+			Thread.sleep(1000);
+			driver.findElement(By.id("cantidadPantalon")).click();
+			driver.findElement(By.id("cantidadPantalon")).sendKeys("1");
+			Thread.sleep(1000);
+			driver.findElement(By.className("boton")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.id("envio")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.id("btnResumen")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.className("continuar")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.id("salir")).click();
+			Thread.sleep(1000);
+
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.quit();
+	}
+	
+	
+
+}
