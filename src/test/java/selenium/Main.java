b1991f78f2d09a468749418ae0288e16c8b0c25b
Kinitto <joaquingomezinfor@gmail.com>
2022-03-01 10:46:30 +0100

    actuclase

--- src/test/java/selenium/Main.java
+++ src/test/java/selenium/Main.java
@@ -2,13 +2,13 @@ package selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.edge.driver", "./drivers/msedgedriver.exe");
-		WebDriver driver = new EdgeDriver();
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriverLinux");
+		WebDriver driver = new ChromeDriver();
 		
 		try {
 			driver.get("http://localhost:8080/");
